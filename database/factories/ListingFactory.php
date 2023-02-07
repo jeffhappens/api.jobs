3f32c340c22882a39e2c2a444a9ffffe060f8ee3
Jeff Mills <blatant3@gmail.com>
2023-02-07 17:09:50 -0500

    Set up factories

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->company(),
+            'company_id' => \App\Models\Company::all()->random()->id,
+            'description' => fake()->sentence( fake()->numberBetween(50, 150) ),
+            'apply_link' => fake()->url()
+        ];
+    }
+}
---
+++ database/migrations/2023_02_07_181029_create_listings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('company_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('apply_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+};
